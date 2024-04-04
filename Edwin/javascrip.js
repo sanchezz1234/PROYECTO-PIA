0bfb709b8f29201eee97e88c3ab6eb31c7d26740
sanchezz1234 <165796819+sanchezz1234@users.noreply.github.com>
2024-04-04 08:53:04 -0500

    Add files via upload

---
+++ Edwin/javascrip.js
@@ -0,0 +1,5 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    // Redireccionar a la página de login/registro
+    window.location.href = 'login_registro.html';
+});
+
